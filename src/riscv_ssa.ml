af3eaa6edb15958388e7232b46575b575b86242c
AdUhTkJm <2292398666@qq.com>
2024-12-20 17:34:42 +0800

    Produces SSA for basic operations

--- src/riscv_ssa.ml
+++ src/riscv_ssa.ml
@@ -31,6 +31,42 @@ type call_data = {
   args: var list;
 }
 
+(**
+Lengths of immediates.
+Other lengths (like 16-bit short) are not supported currently.
+*)
+type imm_type = Bit32 | Bit64
+
+(**
+Assigns (un-)signed integer `imm` to `rd`.
+*)
+type assign_int = {
+  rd: var;
+  imm: int64;
+  size: imm_type;
+  signed: bool;
+}
+
+(**
+Assigns floating point number `imm` to `rd`.
+*)
+type assign_fp = {
+  rd: var;
+  imm: float;
+  size: imm_type;
+}
+
+(**
+Assigns string `imm` to `rd`.
+
+We don't care about how string should be represented;
+that's the job for riscv.ml.
+*)
+type assign_str = {
+  rd: var;
+  imm: string;
+}
+
 type fn = {
   fn: string;
   args: var list;
@@ -45,6 +81,9 @@ and t =
 | Mul of r_type
 | Div of r_type
 | Call of call_data
+| AssignInt of assign_int
+| AssignFP of assign_fp
+| AssignStr of assign_str
 | FnDecl of fn
 | Nop
 
@@ -64,15 +103,39 @@ let to_string t =
     | Sub r -> convert_rtype "sub" r
     | Mul r -> convert_rtype "mul" r
     | Div r -> convert_rtype "div" r
+
     | Call { rd; fn; args } ->
         let args_list = String.concat ", " (List.map to_string args) in
         Printf.sprintf "%s = call %s (%s)" (to_string rd) fn args_list
+
+    | AssignInt { rd; imm; size; signed } ->
+        (* We follow C convention of representing literals. *)
+        let suffix = (match (size, signed) with
+          | (Bit32, true) -> ""
+          | (Bit64, true) -> "ll"
+          | (Bit32, false) -> "u"
+          | (Bit64, false) -> "ull")
+        in 
+        Printf.sprintf "%s = %s%s" (to_string rd) (Int64.to_string imm) suffix
+
+    | AssignFP { rd; imm; size; } ->
+        let suffix = (match size with
+          | Bit32 -> "f"
+          | Bit64 -> "")
+        in
+        Printf.sprintf "%s = %s%s" (to_string rd) (Float.to_string imm) suffix
+    
+    | AssignStr { rd; imm; } ->
+        Printf.sprintf "%s = \"%s\"" (to_string rd) imm
+    
     | FnDecl { fn; args; body; return; } ->
         let args_str = String.concat ", " (List.map to_string args) in
         let body_str = String.concat "\n" (List.map (fun t -> to_str_with_depth t (depth + 1)) body) in
-        let return_str = String.make (depth * 2 + 2) ' ' ^ (to_string return) in
+        let return_str = String.make (depth * 2 + 2) ' ' ^ "return " ^ (to_string return) in
         Printf.sprintf "fn %s (%s) {\n%s\n%s\n}" fn args_str body_str return_str
+    
     | Nop -> "nop"
+  
   in to_str_with_depth t 0
 
 (** Counter of temporaries. *)
@@ -85,16 +148,6 @@ let new_temp ty =
   { name; ty }
 
 (**
-We need to use a mutable structure to generate SSA.
-
-However, this structure cannot be carried as return value,
-since we must return the register in which the result of an SSA instruction is stored.
-
-Therefore the assembly generated is placed in a global variable.
-*)
-let ssa = Basic_vec.make ~dummy:Nop 20
-
-(*
 Currently I don't know what does `prim` ever mean in some places,
 so I ignore them in total.
 
@@ -105,11 +158,11 @@ let warn prim = match prim with
 | Some _ -> prerr_endline "warning: prim is not null"
 
 (**
-As noted above, this function stores the SSA generated in global variable `ssa`.
+This function stores the SSA generated in the given argument `ssa`.
 
-It returns the variable in which 
+It returns the variable in which the result of the last instruction pushed is stored.
 *)
-let do_convert (expr: Mcore.expr) =
+let rec do_convert ssa (expr: Mcore.expr) =
   match expr with
   | Cexpr_var { id; ty; prim; _ } ->
     warn prim;
@@ -117,32 +170,73 @@ let do_convert (expr: Mcore.expr) =
   
   (*
   We treat primitives like special functions.
-  It's just that we prefix them with `__prim_`,
-  in order to distinguish from user-defined functions.
+  
+  Since all primitives start with "%",
+  there is no risk that one of them coincide with user-defined functions.
   *)
   | Cexpr_prim { prim; args; ty; _ } ->
-    let rd = new_temp ty in
-    rd
+      let rd = new_temp ty in
+      let name = Primitive.find_name prim in
+      let args = List.map (fun expr -> do_convert ssa expr) args in
+      let fn = match name with
+        | None -> failwith "riscv_ssa.ml: unrecognized intrinsics"
+        | Some x -> x
+      in
+      Basic_vec.push ssa (Call { rd; fn; args });
+      rd
 
   | Cexpr_apply { func; args; ty; prim; _ } ->
-    warn prim;
-    let rd = new_temp ty in
-    rd
+      warn prim;
+      let rd = new_temp ty in
+      let fn = Ident.to_string func in
+      let args = List.map (fun expr -> do_convert ssa expr) args in
+      Basic_vec.push ssa (Call { rd; fn; args });
+      rd
 
-  | _ -> failwith "TODO: cannot deal with this"
+  | Cexpr_const { c; ty; _ } ->
+      let rd = new_temp ty in
+      let instruction = (match c with
+      | C_string imm ->
+          AssignStr { rd; imm; }
+      | C_bool imm ->
+          AssignInt { rd; imm = Int64.of_int (if imm then 1 else 0); size = Bit32; signed = true; }
+      | C_int { v; _ } ->
+          AssignInt { rd; imm = Int64.of_int32 v; size = Bit32; signed = true; }
+      | C_int64 { v; _ } ->
+          AssignInt { rd; imm = v; size = Bit64; signed = true; }
+      | C_uint { v; _ } ->
+          AssignInt { rd; imm = Int64.of_int32 v; size = Bit32; signed = false; }
+      | C_uint64 { v; _ } ->
+          AssignInt { rd; imm = v; size = Bit64; signed = false; }
+      | C_float { v; _ } ->
+          AssignFP { rd; imm = v; size = Bit32; }
+      | C_double { v; _ } ->
+          AssignFP { rd; imm = v; size = Bit64; }
+      | _ -> failwith "TODO: riscv_ssa.ml: unsupported constant type"
+      ) in
+      Basic_vec.push ssa instruction;
+      rd 
 
+  | _ -> failwith "TODO: riscv_ssa.ml: cannot deal with this expression"
+
+(**
+Converts given `expr` into a list of SSA instructions,
+along with the variable in which the result of this expression is stored.
+*)
 let convert_expr (expr: Mcore.expr) =
-  let return = do_convert expr in
+  let ssa = Basic_vec.make ~dummy:Nop 20 in
+  let return = do_convert ssa expr in
   (Basic_vec.map_into_list ssa (fun x -> x), return)
 
 let convert_toplevel (top: Mcore.top_item) =
   match top with
   | Ctop_expr _ -> []
+
   | Ctop_fn { binder; func; export_info_; loc_ } ->
     let var_of_param ({ binder; ty; _ } : Mcore.param) =
-      { name = Basic_core_ident.to_string binder; ty }
+      { name = Ident.to_string binder; ty }
     in
-    let fn = Basic_core_ident.to_string binder in
+    let fn = Ident.to_string binder in
     let args = List.map var_of_param func.params in
     let (body, return) = convert_expr func.body in
     if export_info_ != None then
@@ -150,11 +244,22 @@ let convert_toplevel (top: Mcore.top_item) =
     [
       FnDecl { fn; args; body; return }
     ]
+  
   | _ -> []
 
 let ssa_of_mcore (core: Mcore.t) =
   Basic_io.write_s "core.ir" (Mcore.sexp_of_t core);
+  (* Deal with other functions *)
   let body = List.map convert_toplevel core.body |> List.flatten in
-  Basic_io.write "core.ssa" (String.concat "\n" (List.map to_string body));
-  body
 
+  (* Deal with main *)
+  let with_main = match core.main with
+    | Some (main_expr, _) ->
+        let (main_body, return) = convert_expr main_expr in
+        let main_decl = FnDecl { fn = "main"; args = []; body = main_body; return } in
+        main_decl :: body
+      
+    | None -> body
+  in
+  Basic_io.write "core.ssa" (String.concat "\n" (List.map to_string with_main));
+  with_main
