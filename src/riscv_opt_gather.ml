7539c9d607b459018a4ad7bd816e0e3711db0fc3
Milky2018 <842376130@qq.com>
2025-05-27 03:19:07 +0000

    Extract some logics to `write_to_file` for convenience.

--- src/riscv_opt_gather.ml
+++ src/riscv_opt_gather.ml
@@ -3,8 +3,7 @@ open Riscv_opt
 open Riscv_ssa
 
 let opt tac =
-  let out_noopt = Printf.sprintf "%s-no-opt.ssa" !Driver_config.Linkcore_Opt.output_file in
-  Basic_io.write out_noopt (String.concat "\n" (List.map Riscv_ssa.to_string tac));
+  Riscv_ssa.write_to_file "-no-opt.ssa" tac;
 
   List.iter (fun top -> match top with
   | FnDecl { fn; args } -> Hashtbl.add params fn args
@@ -12,14 +11,16 @@ let opt tac =
 
   iter_fn2 build_cfg tac;
   let ssa = Riscv_tac2ssa.ssa_of_tac tac in
-  
+
+  Riscv_ssa.write_to_file "-opt0.ssa" ssa;
+
   for i = 1 to 3 do
+    Riscv_ssa.write_to_file (Printf.sprintf "-opt%d.ssa" i) ssa;
     Riscv_opt_inline.inline ssa;
     Riscv_opt_peephole.peephole ssa;
     Riscv_opt_escape.lower_malloc ssa;
   done;
   
   let s = map_fn ssa_of_cfg ssa in
-  let out = Printf.sprintf "%s.ssa" !Driver_config.Linkcore_Opt.output_file in
-  Basic_io.write out (String.concat "\n" (List.map Riscv_ssa.to_string s));
+  write_to_file ".ssa" s;
   s
